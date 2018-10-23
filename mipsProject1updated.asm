afbed188da3be420192669b89bc5b9e2017fdf85
Amari Jones <flameaj@gmail.com>
2018-10-23 15:20:59 -0400

    added J and o
    had to move all my code over to a different file in order to see it on git hub for some reason, all previous commits are on the original mips 1 project and in this one I started printing out my last name

---
+++ mipsProject1updated.asm
@@ -0,0 +1,68 @@
+.data
+begin: .word 'J'
+newLine: .asciiz "/n"
+curr: .word '@'
+comma: .asciiz ","
+
+.text
+main:
+	li $v0,4
+	la $a0, curr
+	syscall 
+	
+	addi $s0, $zero, 0
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	addi $s0, $s0, 2
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	addi $s0, $s0, 6
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	addi $s0, $s0, -2
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	addi $s0, $s0, -6
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	addi $s0, $s0, 6
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	addi $s0, $s0, -5
+	li $v0, 1
+	move $a0, $s0
+	syscall 
+	
+	addi $s0, $s0, 3
+	li $v0, 1
+	move $a0, $s0
+	syscall
+	
+	#Name#
+	#J#
+	li $v0, 4
+        la $a0, begin
+        syscall
+	
+	#o#
+        lw $s1, begin #load the begin var as a word (4bytes)
+        addi $s1, $s1, 37 #load the addition in to a temp register
+        sw $s1, curr #save the value in $t0 into count
+        li $v0, 4 #prepare to load a string
+        la $a0, curr #load the addy of curr into $a0 to print out
+        syscall
+	
+	
+	

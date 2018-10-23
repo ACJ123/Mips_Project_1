4ef3675d8934732987d4aa5deeda8bf45cd66c4c
Amari Jones <flameaj@gmail.com>
2018-10-23 15:53:52 -0400

    Update mipsProject1updated.asm
    added a lower case a to the end of my name

--- mipsProject1updated.asm
+++ mipsProject1updated.asm
@@ -123,6 +123,14 @@ main:
         li $v0, 4 
         la $a0, curr 
         syscall
+        
+        #a#
+        lw $s1, curr
+        addi $s1, $s1, -12
+        sw $s1, curr 
+        li $v0, 4 
+        la $a0, curr 
+        syscall
 	
 	li $v0, 10
    	syscall

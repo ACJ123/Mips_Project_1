68c9d1e54ea3fb32aecce81567352b4273a93551
Amari Jones <flameaj@gmail.com>
2018-10-23 15:47:06 -0400

    Update mipsProject1updated.asm
    added the comma in between my names

--- mipsProject1updated.asm
+++ mipsProject1updated.asm
@@ -91,6 +91,14 @@ main:
         li $v0, 4 
         la $a0, curr 
         syscall
+        
+        #,#
+        lw $s1, curr
+        addi $s1, $s1, -71
+        sw $s1, curr 
+        li $v0, 4 
+        la $a0, curr 
+        syscall
 	
 	li $v0, 10
    	syscall
